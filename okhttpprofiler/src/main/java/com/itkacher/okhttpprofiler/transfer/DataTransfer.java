387593c14a34bce970b5ebcd67ca42f99b14dd13
ievgeniitkachenko <i.tkachenko@rentberry.com>
2018-11-12 11:57:56 +0200

    Add content-type/content-length headers

--- okhttpprofiler/src/main/java/com/itkacher/okhttpprofiler/transfer/DataTransfer.java
+++ okhttpprofiler/src/main/java/com/itkacher/okhttpprofiler/transfer/DataTransfer.java
@@ -1,11 +1,16 @@
 package com.itkacher.okhttpprofiler.transfer;
 
+import java.io.IOException;
+
 import okhttp3.Request;
 import okhttp3.Response;
 
 public interface DataTransfer {
-    void sendRequest(String id, Request request);
-    void sendResponse(String id, Response response);
+    void sendRequest(String id, Request request) throws IOException;
+
+    void sendResponse(String id, Response response) throws IOException;
+
     void sendException(String id, Exception response);
+
     void sendDuration(String id, long duration);
 }
