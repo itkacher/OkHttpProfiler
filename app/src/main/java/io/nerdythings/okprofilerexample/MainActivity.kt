deb2fc7e0aa8b5498d8a8abda9baa81d28a6703d
Eugene Tkachenko <gektor650@gmail.com>
2024-08-05 15:11:51 -0600

    Migrate example to compose

--- app/src/main/java/io/nerdythings/okprofilerexample/MainActivity.kt
+++ app/src/main/java/io/nerdythings/okprofilerexample/MainActivity.kt
@@ -1,7 +1,6 @@
 package io.nerdythings.okprofilerexample
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
